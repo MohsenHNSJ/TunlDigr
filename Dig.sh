72908102b7cb97f67019e8d0402f6d4047227962
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2024-01-10 21:57:05 +0330

    - fix install functions echo on the same line of user input - added the showStartupMessage function with fix - removed -e parameter of all echo functions because it behaves different in some environments

--- Dig.sh
+++ Dig.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-scriptVersion="0.1.3"
+scriptVersion="0.1.4"
 
 askTunnelingMethod() {
 	# We ask the user to select the desired tunneling method
 	# We limit the input character count to 1 by using (-n) argument
-	echo -e "1 - Hysteria 2\n2 - Reality (XTLS VLESS)\n3 - Shadowsocks (Obsolete)"
+	echo "1 - Hysteria 2"
+	echo "2 - Reality (XTLS VLESS)"
+	echo "3 - Shadowsocks (Obsolete)"
 
 	read -n 1 -p "Select tunneling method: " tunnelingMethod
 
@@ -17,7 +19,10 @@ askTunnelingMethod() {
 }
 
 installPackages() {
-	echo -e "=========================================================================\n|       Updating repositories and installing the required packages      |\n|              (This may take a few minutes, Please wait...)            |\n========================================================================="
+	echo "========================================================================="
+	echo "|       Updating repositories and installing the required packages      |"
+	echo "|              (This may take a few minutes, Please wait...)            |"
+	echo "========================================================================="
 	# We update 'apt' repository 
 	# We install/update the packages we use during the process to ensure optimal performance
 	# This installation must run without confirmation (-y)
@@ -25,19 +30,57 @@ installPackages() {
 	sudo apt -y install wget tar openssl gawk sshpass ufw coreutils curl adduser sed grep util-linux qrencode unzip snapd haveged
 }
 
-installHysteria() {
+showStartupMessage() {
+	echo "========================================================================="
+	echo "|                    TunlDigr by @MohsenHNSJ (Github)                   |"
+	echo "========================================================================="
+	echo "Check out the github page, contribute and suggest ideas/bugs/improvments."
+	echo
+	echo "=========================="
+	echo "| Script version $scriptVersion   |"
+	echo "=========================="
+}
 
-	echo "\ninstalling Hysteria"
+installHysteria() {
+	echo
+	echo "installing Hysteria 2"
+
 	# We check and save the latest version number of Sing-Box
-	# latestsingboxversion="$(curl --silent "https://api.github.com/repos/SagerNet/sing-box/releases/latest" | grep -Po "(?<=\"tag_name\": \").*(?=\")"  | sed 's/^.//' )"
+	latestsingboxversion="$(curl --silent "https://api.github.com/repos/SagerNet/sing-box/releases/latest" | grep -Po "(?<=\"tag_name\": \").*(?=\")"  | sed 's/^.//' )"
 }
 
 installReality() {
-	echo "installing Reality"
+	echo "installing Reality (XTLS VLESS)"
 }
 
 installShadowSocks() {
-	echo "installing Shadowsocks"
+	echo "installing ShadowSocks"
+}
+
+optimizeServerSettings() {
+	echo "========================================================================="
+	echo "|                       Optimizing server settings                      |"
+	echo "========================================================================="
+	# We optimise 'sysctl.conf' file for better performance
+	sudo echo "net.ipv4.tcp_keepalive_time = 90" >> /etc/sysctl.conf
+	sudo echo "net.ipv4.ip_local_port_range = 1024 65535" >> /etc/sysctl.conf
+	sudo echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
+	sudo echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+	sudo echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+	sudo echo "fs.file-max = 65535000" >> /etc/sysctl.conf
+
+	# We optimise 'limits.conf' file for better performance
+	sudo echo "* soft     nproc          655350" >> /etc/security/limits.conf
+	sudo echo "* hard     nproc          655350" >> /etc/security/limits.conf
+	sudo echo "* soft     nofile         655350" >> /etc/security/limits.conf
+	sudo echo "* hard     nofile         655350" >> /etc/security/limits.conf
+	sudo echo "root soft     nproc          655350" >> /etc/security/limits.conf
+	sudo echo "root hard     nproc          655350" >> /etc/security/limits.conf
+	sudo echo "root soft     nofile         655350" >> /etc/security/limits.conf
+	sudo echo "root hard     nofile         655350" >> /etc/security/limits.conf
+
+	# We apply the changes
+	sudo sysctl -p
 }
 
 # We get provided arguments
@@ -68,14 +111,7 @@ do
 		fi
 done
 
-echo "=========================================================================
-|                    TunlDigr by @MohsenHNSJ (Github)                   |
-=========================================================================
-Check out the github page, contribute and suggest ideas/bugs/improvments.
-
-==========================
-| Script version $scriptVersion   |
-=========================="
+showStartupMessage
 
 # We check wether the method is supplied at execution or not
 # if not, we ask the user by calling the askTunnelingMethod function
@@ -83,7 +119,7 @@ if [ ! -v tunnelingMethod ]; then
 	askTunnelingMethod
 fi
 
-#installPackages
+installPackages
 
 case $tunnelingMethod in
 	1)
