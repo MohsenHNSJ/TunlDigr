1e479d8b5c5b69a74e5bd48487fcd5f787b2c80d
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2024-01-10 19:23:19 +0330

    - added required packages for shadowsocks and reality - moved package updating to a seprate function for better code readability - added a warning message for -tm argument if the input variable is invalid - moved script version variable to the top of the script for better acessibility and visibility - moved startup message to a seprate function for cleaner code - moved processing of input arguments to a seprate function for cleaner code

--- Dig.sh
+++ Dig.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+scriptVersion="0.1.2"
+
 askTunnelingMethod() {
 	# We ask the user to select the desired tunneling method
 	# We limit the input character count to 1 by using (-n) argument
@@ -11,7 +13,56 @@ askTunnelingMethod() {
 	# We validate user input
 	until [[ $tunnelingMethod == +([1-3]) ]]; do
 		echo
-		read -n 1 -p "Wrong input, please only input a number from 1 - 3: " tunnelingMethod
+		read -n 1 -p "Invalid input, please only input a number from 1 - 3: " tunnelingMethod
+	done
+}
+
+installPackages() {
+	echo "=========================================================================
+	|       Updating repositories and installing the required packages      |
+	|              (This may take a few minutes, Please wait...)            |
+	========================================================================="
+	# We update 'apt' repository 
+	# We install/update the packages we use during the process to ensure optimal performance
+	# This installation must run without confirmation (-y)
+	sudo apt update
+	sudo apt -y install wget tar openssl gawk sshpass ufw coreutils curl adduser sed grep util-linux qrencode unzip snapd haveged
+}
+
+showStartupMessage() {
+	echo "=========================================================================
+	|                    TunlDigr by @MohsenHNSJ (Github)                   |
+	=========================================================================
+	Check out the github page, contribute and suggest ideas/bugs/improvments.
+	
+	==========================
+	| Script version $scriptVersion   |
+	=========================="
+}
+
+evaluateArguments() {
+	# We iterate all arguments
+	while [ $i -le $totalArguments ]
+	do 
+		i=$((i + 1));
+		# We check for tunneling method argument
+		if  [ $1 = "-tm" ]; then
+			shift
+			# Hysteria 2
+			if [ $1 == "h2" ]; then
+				tunnelingMethod=1
+			fi
+			# Reality
+			if [ $1 == "xr" ]; then
+				tunnelingMethod=2
+			fi
+			# Shadowsocks
+			elif [ $1 == "ss" ]; then
+				tunnelingMethod=3
+			# Invalid input
+			else 
+				echo "Invalid input for argument -tm. ignoring"		
+			fi
 	done
 }
 
@@ -19,37 +70,9 @@ askTunnelingMethod() {
 i=1;
 totalArguments=$#
 
-# We iterate all arguments
-while [ $i -le $totalArguments ]
-do 
-	i=$((i + 1));
-	# We check for tunneling method argument
-	if  [ $1 = "-tm" ]; then
-		shift
-		# Hysteria 2
-		if [ $1 == "h2" ]; then
-			tunnelingMethod=1
-		fi
-		# Reality
-		if [ $1 == "xr" ]; then
-			tunnelingMethod=2
-		fi
-		# Shadowsocks
-		elif [ $1 == "ss" ]; then
-			tunnelingMethod=3
-		fi
-done
+evaluateArguments
 
-scriptVersion="0.1.1"
-
-echo "=========================================================================
-|                    TunlDigr by @MohsenHNSJ (Github)                   |
-=========================================================================
-Check out the github page, contribute and suggest ideas/bugs/improvments.
-
-==========================
-| Script version $scriptVersion   |
-=========================="
+showStartupMessage
 
 # We check wether the method is supplied at execution or not
 # if not, we ask the user by calling the askTunnelingMethod function
@@ -57,12 +80,7 @@ if [ ! -v tunnelingMethod ]; then
 	askTunnelingMethod
 fi
 
-echo "=========================================================================
-|       Updating repositories and installing the required packages      |
-|              (This may take a few minutes, Please wait...)            |
-========================================================================="
-# We update 'apt' repository 
-# We install/update the packages we use during the process to ensure optimal performance
-# This installation must run without confirmation (-y)
-sudo apt update
-sudo apt -y install wget tar openssl gawk sshpass ufw coreutils curl adduser sed grep util-linux qrencode
+installPackages
+
+echo
+echo tunnelingMethod
